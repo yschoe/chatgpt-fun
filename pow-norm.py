8552e0ae719faffabc38ee2c04b91bad4eda178f
unknown <yschoe@gmail.com>
2024-11-13 15:28:02 -0600

    linear bins plotted log-log

--- pow-norm.py
+++ pow-norm.py
@@ -1,19 +1,9 @@
-'''
-
-  comparing half-normal distribution with power-law distribution with 
-  the same standard deviation.
-
-  https://chatgpt.com/share/6734eee1-a9b4-8003-88c0-b835291ba6f8
-
-'''
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import halfnorm
 
 # Set parameters
-num_samples = 100000
+num_samples = 10000
 alpha = 10.0  # Power-law exponent (typically > 1)
 min_value = 0.001  # Lower bound for the power-law distribution
 
@@ -41,29 +31,31 @@ half_normal_samples_scaled = half_normal_samples * scaling_factor
 mean_shift = power_law_mean - np.mean(half_normal_samples_scaled)
 half_normal_samples_scaled_shifted = half_normal_samples_scaled + mean_shift
 
-# Define more histogram bins based on the combined range of the data
+# Define linear bins based on the combined range of both distributions
 all_samples = np.concatenate((power_law_samples, half_normal_samples_scaled_shifted))
 num_bins = 300
 bins = np.linspace(np.min(all_samples), np.max(all_samples), num_bins)
 
-# Compute histograms for each distribution to find the 0.1 density threshold
+# Compute histogram densities (in linear scale) for both distributions
 power_law_hist, bin_edges = np.histogram(power_law_samples, bins=bins, density=True)
 half_normal_hist, _ = np.histogram(half_normal_samples_scaled_shifted, bins=bins, density=True)
 
-# Find the range where density is greater than 0.1 in either distribution
+# Define bin centers for plotting
 bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-mask = (power_law_hist > 0.1) | (half_normal_hist > 0.1)
-plot_min, plot_max = bin_centers[mask][0], bin_centers[mask][-1]
 
-# Plot the two scaled and shifted distributions using the same bins, restricting the range
+# Plot the histograms in log-log scale
 plt.figure(figsize=(12, 6))
-plt.hist(power_law_samples, bins=bins, density=True, alpha=0.5, color='red', label='Power-Law Distribution')
-plt.hist(half_normal_samples_scaled_shifted, bins=bins, density=True, alpha=0.5, color='blue', label='Scaled & Shifted Half-Normal Distribution')
-plt.xlim(plot_min, plot_max)
+plt.plot(bin_centers, power_law_hist, color='red', label='Power-Law Distribution', alpha=0.7)
+plt.plot(bin_centers, half_normal_hist, color='blue', label='Scaled & Shifted Half-Normal Distribution', alpha=0.7)
 
 # Set both axes to log scale
 plt.xscale('log')
 plt.yscale('log')
+
+# Limit x-axis to relevant range where density is above 0.1
+mask = (power_law_hist > 0.1) | (half_normal_hist > 0.1)
+plot_min, plot_max = bin_centers[mask][0], bin_centers[mask][-1]
+plt.xlim(plot_min, plot_max)
 
 # Add title with empirical standard deviations
 plt.xlabel('Value')
@@ -72,3 +64,4 @@ plt.legend()
 plt.title(f'Empirical Distributions: Power-Law vs. Scaled & Shifted Half-Normal (Log-Log Plot)\n'
           f'Std Dev (Power-Law): {power_law_std:.3f} | Std Dev (Scaled Half-Normal): {half_normal_std:.3f}')
 plt.show()
+
